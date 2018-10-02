a4040ea54c695dca39d822270dfeb173e4c3f6dd
t0shifollow <marcindymek24@gmail.com>
2018-10-02 17:42:23 +0200

    Progress..

--- dist/client.js
+++ dist/client.js
@@ -1,3 +1,14 @@
-console.log('WUT');
-
 const form = document.querySelector('form');
+
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const user = {
+        name,
+            content
+    }
+    console.log(user);
+});
